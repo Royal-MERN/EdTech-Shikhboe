6a81d8b3ecca7ee1234fba0bc1d8be2f08c0f907
Showrove Roy <showrove200@gmail.com>
2023-02-15 01:43:51 +0600

    404 error page responsive done & added a button

--- src/Pages/404Error/Error404.jsx
+++ src/Pages/404Error/Error404.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SquareBtn from "../../Components/Global/Buttons/SquareBtn";
 import Footer from "../../Components/Global/Footer/Footer";
 import NavBar from "../../Components/Global/NavBar/NavBar";
 
@@ -6,16 +7,17 @@ const Error404 = () => {
   return (
     <>
       <NavBar></NavBar>
-      <div className='bg-[#FDFDE9] w-screen pb-10'>
-        <div className='flex justify-center items-center flex-col'>
-          <h2 className='capitalize font-semibold text-4xl pt-10'>
+      <div className='bg-[#FDFDE9] w-full h-screen pb-10 flex justify-center items-center flex-col emaxW'>
+        <div className='flex justify-center items-center flex-col maxW'>
+          <h2 className='capitalize font-semibold sm:text-4xl pt-10 text-center text-primary'>
             404 Page is not found
           </h2>
           <img
-            className='w-1/2'
+            className='w-1/2 pb-5'
             src='../../../public/images/Glasses.gif'
             alt=''
           />
+          <SquareBtn to='courses'>Courses</SquareBtn>
         </div>
       </div>
       <Footer></Footer>
