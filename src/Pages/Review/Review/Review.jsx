e04fc1c0ad0182f4500b861d7376af8ea53d81e1
Masraful Zishan <zishan3231>
2023-02-16 14:27:01 +0600

    add

--- src/Pages/Review/Review/Review.jsx
+++ src/Pages/Review/Review/Review.jsx
@@ -4,23 +4,23 @@ import RightSide from '../Right-Bar/RightSide';
 
 const Review = () => {
     return (
-        <div className='emaxW mt-8'>
-           <div className='grid grid-cols-3 gap-10'>
-            {/* Left-bar-start */}
-            <div className='col-span-2'>
-            <LeftSide></LeftSide>
+        <div className='maxW mt-8 pb-28'>
+            <div className='grid grid-cols-3 gap-10'>
+                {/* Left-bar-start */}
+                <div className='col-span-2'>
+                    <LeftSide></LeftSide>
+                </div>
+                {/* Left-bar-end */}
+                {/* Right-bar-start */}
+                <div>
+                    <div className='sticky top-[100px]'>
+
+                        <RightSide></RightSide>
+                    </div>
+
+                </div>
+                {/* Right-bar-end */}
             </div>
-            {/* Left-bar-end */}
-            {/* Right-bar-start */}
-<div>
-    <div className='sticky top-[100px]'>
-
-    <RightSide></RightSide>
-    </div>
-
-</div>
-{/* Right-bar-end */}
-           </div>
         </div>
     );
 };
