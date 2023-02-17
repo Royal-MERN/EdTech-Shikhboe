4ba5a475166315fa4abf7b46532adf23439e95e9
Showrove Roy <showrove200@gmail.com>
2023-02-17 21:03:40 +0600

    privacy-policy  page add

---
+++ src/Pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+    return (
+        <div className='maxW py-10'>
+            <h2 className='text-center md:text-3xl text-xl font-medium text-text2'>Privacy Policy</h2>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
--- src/Routes/Route/Route.jsx
+++ src/Routes/Route/Route.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../../Layout/MainLayout/MainLayout";
 import Error404 from "../../Pages/404Error/Error404";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
+import PrivacyPolicy from "../../Pages/PrivacyPolicy/PrivacyPolicy";
 import Register from "../../Pages/Register/Register";
 import Test from "../../Pages/test/Test";
 
@@ -10,25 +11,28 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-    errorElement:<Error404></Error404>,
+    errorElement: <Error404></Error404>,
     children: [
       {
         path: "/",
         element: <Home></Home>,
       },
       {
-        path:'/test',
-        element:<Test></Test>
-      }
+        path: "/test",
+        element: <Test></Test>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/privacy-policy",
+        element: <PrivacyPolicy></PrivacyPolicy>,
+      },
     ],
   },
-  
-  {
-    path:'/login',
-    element:<Login></Login>
-  },
-  {
-    path:'/register',
-    element:<Register></Register>
-  }
 ]);
