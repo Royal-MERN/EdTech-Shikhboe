2fcec4ff6f6bbede77f03de78ad4cc44f53a80a3
Showrove Roy <showrove200@gmail.com>
2023-02-17 20:16:51 +0600

    Whatsapp chat added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./Components/AuthProvider/AuthProvider";
+import WAChat from "./Components/Global/WAChat/WAChat";
 
 import "./index.css";
 import { router } from "./Routes/Route/Route";
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
     <RouterProvider router={router}></RouterProvider>
+    <WAChat/>
     </AuthProvider>
   </React.StrictMode>
 );
