0a3c3469fe0e93dd3538bd1f11eab886417b32fd
Showrove Roy <showrove200@gmail.com>
2023-02-25 00:44:20 +0600

    profile details input field added

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 import { FiCamera } from "react-icons/fi";
 import { FaUserCircle } from "react-icons/fa";
 import UserAvatar from "../../Components/UserAvatar/UserAvatar";
+import { useForm } from "react-hook-form";
 
 const Profile = () => {
   const [profileImage, setProfileImage] = useState(null);
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    const userInfo = {
+      fullName: data.fullName,
+      certificateName: data.certificateName,
+      emailAddress: data.emailAddress,
+      phoneNumber: data.phoneNumber,
+      avatarUrl: profileImage,
+    };
+    console.log(userInfo);
+  };
   return (
-    <div className='maxW'>
+    <div className='maxW py-10'>
       <div className='max-w-3xl mx-auto'>
         <h2 className='text-2xl font-medium'>প্রোফাইল</h2>
         <div className='flex justify-center my-5'>
@@ -14,7 +26,6 @@ const Profile = () => {
             <label
               htmlFor='userAvatar'
               className='absolute z-10 -right-2 -top-1 rounded-full text-white bg-slate-700 p-1 cursor-pointer'>
-              {/* <input type="file" className="file-input opacity-0 absolute w-5 h-5 cursor-pointer" /> */}
               <FiCamera className='cursor-pointer'></FiCamera>
             </label>
             {profileImage ? (
@@ -34,6 +45,54 @@ const Profile = () => {
         {!profileImage && (
           <UserAvatar setProfileImage={setProfileImage}></UserAvatar>
         )}
+        <div className='max-w-xl mx-auto'>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text text-lg'>Full Name</span>
+            </label>
+            <input
+              {...register("fullName")}
+              type='text'
+              placeholder='Your Full Name'
+              className='input input-bordered w-full rounded'
+            />
+            <label className='label pt-5'>
+              <span className='label-text text-lg'>Certificate Name</span>
+            </label>
+            <input
+              {...register("certificateName")}
+              type='text'
+              placeholder='Type Your Certificate Name'
+              className='input input-bordered w-full rounded'
+            />
+            <label className='label pt-5'>
+              <span className='label-text text-lg'>Email</span>
+            </label>
+            <input
+              {...register("emailAddress")}
+              type='text'
+              placeholder='Type your email'
+              className='input input-bordered w-full rounded'
+            />
+            <label className='label pt-5'>
+              <span className='label-text text-lg'>Phone Number</span>
+            </label>
+            <input
+              {...register("phoneNumber")}
+              type='text'
+              placeholder='Type your Phone Number'
+              className='input input-bordered w-full rounded'
+            />
+
+            <input
+              type='submit'
+              value='Save'
+              className='btn btn-primary mt-5 w-28 mx-auto'
+            />
+          </form>
+        </div>
       </div>
     </div>
   );
